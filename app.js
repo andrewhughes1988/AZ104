af6aab9cc5b75cce4deb9303cb7bf3188c1c6170
Andrew Hughes <andrewhughes1988@gmail.com>
2025-08-14 20:04:32 -0500

    initial

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 80;
+const os = require('os');
+
+const hostname = os.hostname();
+
+app.get('/', (req, res) => {
+  res.send('AZURE APP SERVICE - Node.js App is running!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://${hostname}:${port}`);
+});
