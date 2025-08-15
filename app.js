a5d3afdbb9e02d98caa41fffc4716c1a0635ab30
Andrew Hughes <andrewhughes1988@gmail.com>
2025-08-14 20:27:44 -0500

    updated app.js to dynamically allocate port for Azure use

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //Basic App for deploying to App Service in Azure
 const express = require('express');
 const app = express();
-const port = 80;
+const port = process.env.PORT || 80;
 const os = require('os');
 
 const hostname = os.hostname();
